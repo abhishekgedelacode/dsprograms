2b6026fadcd2a61183aa494a29908fd08e1719d1
abhishekgedelacode <gabhishekji143@gmail.com>
2020-12-13 21:05:26 +0530

    added stack using linked list that in ds lab program

---
+++ dsLab/liststack.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+  int data;
+  strcut node *ptr;
+} * top, *temp, *top1;
+
+int topelement();
+void push(int);
+void pop();
+void empty();
+void display();
+void elecount();
+void create();
+
+int count = 0;
+
+void main()
+{
+  int data, ch;
+
+  printf("\n 1.push \n 2.pop \n 3.top element \n 4.display \n 5.count \n 6.exit \n");
+
+  create();
+
+  while (1)
+  {
+    printf("\n enter your choice: ");
+    scanf("%d", &ch);
+
+    if (ch == 1)
+    {
+      printf("enter data: ");
+      scanf("%d", &data);
+
+      push(data);
+    }
+    else if (ch == 2)
+    {
+      pop();
+    }
+    else if (ch == 3)
+    {
+      if (top == NULL)
+      {
+        printf("\n stack is empty \n ");
+      }
+      else
+      {
+        printf("\n top element: %d \n", topelement());
+      }
+    }
+    else if (ch == 4)
+    {
+      display();
+    }
+    else if (ch == 5)
+    {
+      elecount();
+    }
+    else if (ch == 6)
+    {
+      exit(0);
+    }
+    else
+    {
+      printf("\n invalid choice \n");
+    }
+  }
+}
+
+void create()
+{
+  top = NUll;
+}
+
+void push(int data)
+{
+  if (top == NULL)
+  {
+    top = (struct node *)malloc(sizeof(struct node));
+    top->ptr = NULL;
+    top->data = data;
+  }
+  else
+  {
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->ptr = top;
+    temp->data = data;
+    top = temp;
+  }
+  count++;
+}
+
+void pop()
+{
+  top1 = top;
+
+  if (top1 == NULL)
+  {
+    printf("\n stcack empty \n");
+  }
+  else
+  {
+    top1 = top1->ptr;
+    printf("\n popped data: %d \n", top->data);
+    free(top);
+    top = top1;
+    count--;
+  }
+}
+
+int topelement()
+{
+  return top->data;
+}
+
+void display()
+{
+  top1 = top;
+
+  if (top1 == NULL)
+  {
+    printf("\n stack is empty \n");
+    return;
+  }
+
+  while (top1 != NULL)
+  {
+    printf(" %d -> ", top1->data);
+    top1 = top1->ptr;
+  }
+}
+
+void elecount()
+{
+  printf("\n stack size: %d\n", count);
+}
